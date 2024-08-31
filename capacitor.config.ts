da280a8777c0187fa0999b5a384ea1eb22852d18
kocha <kocha@Kocha.local>
2024-08-31 17:11:36 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-responsive-starter',
+  webDir: 'dist'
+};
+
+export default config;
